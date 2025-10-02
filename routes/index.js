312d9125c49c64c89a59de43db74a3ad1590cb69
raphaeltaurai <shawntaurai@gmil.com>
2025-10-02 05:14:59 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,13 @@
 const routes = require('express').Router();
 const client = require('./clients');
-const employee = require('./employees');
 
 routes.use('/clients', client);
-routes.use('/employees', employee);
 routes.get('/', (req, res) => {
   res.send({
     message: 'Hello World! Welcome to the Clients API',
     documentationURL: 'http://localhost:8000/api-docs',
     endpoints: {
       clients: '/clients',
-      employees: '/employees',
       swagger: '/api-docs'
     }
   });
