3ef61b5217ecfd7df27e98c93998e100c3693db9
raphaeltaurai <shawntaurai@gmil.com>
2025-10-07 04:40:30 +0200

    personalisation function

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,38 @@ routes.get('/', (req, res) => {
   const protocol = req.protocol;
   const host = req.get('host');
   const isAuthed = req.isAuthenticated && req.isAuthenticated();
-  res.send({
-    message: 'Hello World! Welcome to the Clients API',
-    authenticated: !!isAuthed,
-    user: isAuthed ? { id: req.user && req.user.id, email: req.user && req.user.email, displayName: req.user && req.user.displayName } : null,
-    loginURL: isAuthed ? null : `${protocol}://${host}/auth/github`,
-    logoutURL: isAuthed ? `${protocol}://${host}/auth/logout` : null,
-    documentationURL: `${protocol}://${host}/api-docs`,
-    endpoints: {
-      clients: '/clients',
-      swagger: '/api-docs'
-    }
-  });
+  
+  if (isAuthed) {
+    // User is logged in - show personalized welcome
+    const userName = req.user && req.user.displayName || req.user && req.user.email || 'User';
+    res.send({
+      message: `Welcome back, ${userName}! You are successfully logged in.`,
+      authenticated: true,
+      user: { 
+        id: req.user && req.user.id, 
+        email: req.user && req.user.email, 
+        displayName: req.user && req.user.displayName 
+      },
+      logoutURL: `${protocol}://${host}/auth/logout`,
+      documentationURL: `${protocol}://${host}/api-docs`,
+      endpoints: {
+        clients: '/clients',
+        swagger: '/api-docs'
+      }
+    });
+  } else {
+    // User is not logged in - show login options
+    res.send({
+      message: 'Welcome to the Clients API! Please log in to access protected features.',
+      authenticated: false,
+      loginURL: `${protocol}://${host}/auth/github`,
+      documentationURL: `${protocol}://${host}/api-docs`,
+      endpoints: {
+        clients: '/clients',
+        swagger: '/api-docs'
+      }
+    });
+  }
 });
 
 module.exports = routes;
